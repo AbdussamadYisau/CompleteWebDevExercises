8cf151923d80f6c2ec2935de7f92a1576fb8dbd8
AbdussamadYisau <Sammieyisau@gmail.com>
2019-10-12 23:32:38 +0100

    Used Objects and Loops rather well, I would say

---
+++ JavaScript/DataStructure/DatabaseAndTimelineWithObjectsAndLoops/script.js
@@ -0,0 +1,65 @@
+var database = [
+
+    {
+        username: "andrei",
+        password: "supersecret"
+    },
+    
+    {
+       username: "Michael",
+       password:"scofield"
+    },
+
+    {
+    	username: "Salamat",
+    	password: "HpEnvy"
+    }
+
+];
+
+
+var newsfeed = [
+
+      {
+      	username: "Bobby Razzmatazz",
+      	password: "So tired from all that JS!"
+      }, 
+
+      {
+      	username: "Sally", 
+      	password: "JavaScript is so exhausting"
+      },
+
+      {
+      	username: "Mitch",
+      	password: "What a sharp intuition you\'ve got"
+      }
+
+];
+
+function isUserValid(username, password) {
+	 for(var i = 0; i < database.length; i++) {
+		if(database[i].username === username  && database[i].password === password) {
+
+			return true;
+		}
+	
+	}
+	return false;
+}
+
+function signIn(username, password) {
+	// console.log(isUserValid(username,password)); - used to check results of previous function
+    if (isUserValid(username,password)) {
+    	console.log(newsfeed);
+    } else {
+    	alert("Sorry, wrong username and/or password 😢");
+    }
+
+}
+
+
+var usernamePrompt = prompt("What\'s your username?");
+var passwordPrompt = prompt("What\'s your password?");
+
+signIn(usernamePrompt,passwordPrompt);
