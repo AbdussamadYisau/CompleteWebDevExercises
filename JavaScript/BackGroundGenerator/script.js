b6ad5191c14da5311df99ff954b48a79424b2fee
AbdussamadYisau <Sammieyisau@gmail.com>
2019-10-17 22:57:23 +0100

    Added copy to clip board feature, would work on beautifying later

--- JavaScript/BackGroundGenerator/script.js
+++ JavaScript/BackGroundGenerator/script.js
@@ -18,9 +18,20 @@ color2.addEventListener("input", setGradient);
 
 
 function copText() {
-  var copyText = document.getElementsByTagName("h3")[0];
-  copyText[0].select();
-  copyText[0].setSelectionRange(0, 99999)
+  // var copyText = document.getElementsByTagName("h3")[0];
+  // copyText[0].select();
+  // copyText[0].setSelectionRange(0, 99999)
+  // document.execCommand("copy");
+  // alert("Copied the text: " + copyText.value);
+
+  var p1 = document.getElementById("p1");
+  // Set color with the hidden field so that you can call select on it
+
+  var hiddenField = document.getElementById("copyText");
+  hiddenField.value = p1.innerHTML;
+  hiddenField.select();
   document.execCommand("copy");
-  alert("Copied the text: " + copyText.value);
+    
+  alert("Copied the text: " + hiddenField.value);
+
 }
