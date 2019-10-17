54b3a64f7bc72aeddb96d4faaa1f3d04515363e4
AbdussamadYisau <Sammieyisau@gmail.com>
2019-10-17 19:58:47 +0100

    Background generator

---
+++ JavaScript/BackGroundGenerator/script.js
@@ -0,0 +1,26 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background =
+	 "linear-gradient(to right, "+ color1.value + ","+ color2.value+")";
+      
+      // Tells us the colors we choose
+      css.textContent = body.style.background  + ";";
+
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+function copText() {
+  var copyText = document.getElementsByTagName("h3")[0];
+  copyText[0].select();
+  copyText[0].setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
