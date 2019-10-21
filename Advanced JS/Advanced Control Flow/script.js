272aa16abd4742cb730d77c2f6cab16c6d9df2e8
AbdussamadYisau <Sammieyisau@gmail.com>
2019-10-21 23:10:13 +0100

    Learnt about ternary operator and switch, that is to say if-else that went to the university.

---
+++ Advanced JS/Advanced Control Flow/script.js
@@ -0,0 +1,36 @@
+// function isUserValid(bool) {
+// 	// body...
+
+// 	return bool;
+// }
+
+// var answer = isUserValid(true) ? "You may enter" : "You shall not pass";
+
+function moveCommand(direction) {
+	// body...
+	var whatHappens;
+
+	switch(direction) {
+      case "forward":
+      whatHappens = "you encounter a monster";
+       break;
+
+       case "backward":
+       whatHappens = "you encounter dragons and trolls";
+       break; 
+       
+       case "right" : 
+       whatHappens = "you encounter Ifunanya ";
+       break;
+
+       case "left" : 
+       whatHappens = "you encounter Ife";
+       break;
+
+       default:
+        whatHappens = "please enter a valid direction";
+
+	}
+
+	return whatHappens;
+}
