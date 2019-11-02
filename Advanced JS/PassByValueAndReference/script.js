1b9be8c0045eb5efd5484ad3f60bd369e1031056
AbdussamadYisau <Sammieyisau@gmail.com>
2019-11-02 02:05:31 +0100

    shallow cloning, deep cloning and everything nice

--- Advanced JS/PassByValueAndReference/script.js
+++ Advanced JS/PassByValueAndReference/script.js
@@ -21,3 +21,76 @@ obj2.password = "easypeasy";
 
 console.log("Object 1", obj1);
 console.log("Object 2", obj2);
+
+let c = [1, 2, 3, 4 , 5];
+
+let d = c;
+
+d.push(6);
+
+console.log(c); // Proving that arrays are objects in make up. What if you want to copy an array by value? Simple
+
+let e = [].concat(c);
+
+e.push(7);
+
+console.log("Passed by Value Array", e); // Okay, nice. What about for an actual object?
+// Appaz, there are two ways of doing this.
+let obj = {
+	a: "a",
+	b: "b",
+	c: "c", 
+	d: "d",
+	e: "e"
+};
+
+// 1 - way
+let clonedObj = Object.assign({}, obj);
+
+console.log("The cloned object: " , clonedObj);
+
+// 2 - way
+
+let clonedObj2 = {...obj};
+
+console.log("The second cloned object", clonedObj2);
+
+//checking original object after modifying a property
+
+obj.a = 1;
+
+console.log("Original obj", obj);
+
+
+
+let firstObj = {
+	a: "a",
+	b: "b",
+	c: "c", 
+	d: "d",
+	e: {
+		deep: "try and copy me"
+	}
+};
+
+let cloneFirstObj = Object.assign({}, firstObj);
+
+let cloneFirstObj2 = {...firstObj}
+
+//Deep Cloning
+
+let superClone = JSON.parse(JSON.stringify(firstObj));
+
+firstObj.a = 5, firstObj.c = "memory";
+
+firstObj.e.deep = "wassup?";
+
+console.log(firstObj);
+console.log(cloneFirstObj);
+console.log(cloneFirstObj2);
+
+//The deep property of the clones are affected as well, cause their cloning was shallow i.e first layer cloning 
+
+console.log(superClone);
+
+
