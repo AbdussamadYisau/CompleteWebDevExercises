76a924cb4c5682ae585edd9f7effc633a9ffd821
AbdussamadYisau <Sammieyisau@gmail.com>
2019-11-03 01:18:59 +0100

    ES7

---
+++ Advanced JS/ES7/script.js
@@ -0,0 +1,14 @@
+// Only two major additions/changes
+
+ // One, .includes() / The includes() method determines whether an array or a string includes a certain value among its entries, returning true or false as appropriate.
+
+ "Helloooooo".includes("o"); // returns true
+
+ const pets = ["cat", "dog", "bat"];
+
+ pets.includes("dog"); //  returns true
+
+// Two, exponential operator
+
+const square = (x) => (x**2);
+const cube = (y) => (y**3);
