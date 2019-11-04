4ad6ef84eccebe24e52b6c45bbed1715e99396de
AbdussamadYisau <Sammieyisau@gmail.com>
2019-11-04 09:13:03 +0100

    Added exercise

---
+++ Advanced JS/ES8/exercise.js
@@ -0,0 +1,35 @@
+// Solve the below problems:
+
+// #1) Line up the Turtle and the Rabbit at the start line:
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+
+// it should look like this:
+'     ||<- Start line'
+'       🐢'
+'       🐇'
+
+// when you do:
+console.log(startLine.padStart(8));
+console.log(turtle.padStart(8));
+console.log(rabbit.padStart(8));
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+  '🐢======='
+
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ');
