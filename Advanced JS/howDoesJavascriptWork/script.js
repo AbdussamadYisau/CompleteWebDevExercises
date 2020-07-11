a4672bba5f86c35f167ecd1767ca64e0be2ac306
AbdussamadYisau <Sammieyisau@gmail.com>
2020-07-11 20:58:18 +0100

    Added method to form

--- Advanced JS/howDoesJavascriptWork/script.js
+++ Advanced JS/howDoesJavascriptWork/script.js
@@ -2,7 +2,11 @@
 
 const a = 1; // We've just allocated memory. 
 
-// Memory Leak occurs when we have unused memory (allocated) just laying around, which causes the memory heap t fill and rendres the browser unable to work. This why some are of the opinion that Global Variables are bad cause if we forget to clean them up, it leads to Memory Leak. You get? Like the variables local to functions get deleted after the function has been called. So, you see? Do you? 
+// Memory Leak occurs when we have unused memory (allocated) just laying around, 
+// which causes the memory heap to fill and renders the browser unable to work. 
+//This why some are of the opinion that Global Variables are bad cause if we forget to clean them up, 
+// it leads to Memory Leak. You get? 
+//Like the variables local to functions get deleted after the function has been called. So, you see? Do you? 
 
 
 // Call Stack - reads and executes our scripts.
@@ -28,3 +32,15 @@ console.log(fun());
 // }
 
 // foo();
+
+
+// Asynchronous Programming
+
+console.log(5);
+
+setTimeout(() =>{
+	console.log("2");
+}, 2000); // this runs after 2000 ms, that is 2s
+
+console.log("3");
+
